6adb7f4fc9f922adc7f350387eb4eb5cb617e130
SreckoStojic <stojic.srecko@gmail.com>
2021-10-13 13:45:49 +0200

    port changed to 443 for https

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/api/users', (req, res) => {
     res.send();
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
